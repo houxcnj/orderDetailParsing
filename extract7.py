2e19f8c7c42d1e7729a4e0df0253a278584a7745
houxcnj <houxcnj@hotmail.com>
2016-08-12 15:45:16 -0700

    feat: ectract from report

---
+++ extract7.py
@@ -0,0 +1,36 @@
+import os
+import json
+
+def extractTracking(file):
+
+	f = open(file, "r")
+
+	lines = f.readlines()
+
+	res = []
+
+	for line in lines:
+		info = line.split("\t")
+		thisLine = (info[0], [info[-6], info[-5]])
+		res.append(thisLine)
+
+	f.close()
+
+	return dict(res)
+
+def toJson():
+	f = open("tracking.json", "w")
+	tracking = extractAll()
+	json.dump(tracking, f, indent = 2, ensure_ascii = False)
+	f.close()
+
+def extractAll():
+	res = {}
+	trackingDir =  os.path.join(os.path.dirname(__file__), 'tracking\\')
+	for file in os.listdir(trackingDir):
+		if file.endswith(".txt"):
+			res.update(extractTracking(trackingDir+file))
+
+	return res
+
+toJson()
