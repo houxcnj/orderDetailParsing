a4927a4cbba47cc3378b15c59b35d7f0440d6744
houxcnj <houxcnj@hotmail.com>
2016-11-23 15:06:29 -0800

    fix: bugs in extractRefund

--- extractRefund.py
+++ extractRefund.py
@@ -15,9 +15,12 @@ def getRefund(lines):
 			lineDict = {l[13]:l[14]}
 			refundDict[l[7]] = lineDict
 		else:
+			if l[13] in lineDict:
+				l[13] = l[13] + "2"
 			lineDict[l[13]] = l[14]
+				
 	return refundDict
-
+"""
 def orderInfo(rfile, lines):
 	f = open(rfile, "r")
 	lines2 = f.readlines()
@@ -26,18 +29,46 @@ def orderInfo(rfile, lines):
 	orderNo = []
 	for line in lines:
 		l = line.split("\t")
+		# print l[7], l[8]
 		orderNo.append(l[7])
 
 	res = []
+	lines2[:] = [x for x in lines2 if "Order" in x]
 	for order in set(orderNo):
+		print order
 		for line in lines2:
-			thisLine = line.split("\t")
-			if order == thisLine[8] and "Order" in line:
-				
-				thisLine[13] = "Original_"+thisLine[13]
-				res.append("\t".join(thisLine))
+			if order in line:
+				res.append(line)
 	return res
+"""
+def getOriOd(lines, refundDict):
+	if not lines:
+		return None
+	lineDict = {}
+	for line in lines:
+		l = line.split("\t")
+		l[13] = "Ori_" + l[13]
+		if l[7] in refundDict:
+			if l[13] in refundDict[l[7]]:
+				l[13] = l[13] + "2"
+			lineDict = {l[13]:l[14]}
+			refundDict[l[7]].update(lineDict)
+			
+	return refundDict
 
+
+def orderInfo(rfile, refundDict):
+	f = open(rfile, "r")
+	lines = f.readlines()
+	f.close()
+
+	lines[:] = [x for x in lines if "Order" in x]
+	for line in lines:
+		l = line.split("\t")
+		if not l[7] in refundDict:
+			lines.remove(line)
+
+	return lines
 
 def refundInfo(rfile):
 	f = open(rfile, "r")
@@ -49,12 +80,12 @@ def refundInfo(rfile):
 	return lines
 
 def toJson():
-	refundDict = extractAll()
+	refundDict = extractRefOds()
 	f = open("refund.json", "w")
 	json.dump(refundDict, f, indent = 2, ensure_ascii = False)
 	f.close()
 
-def extractAll():
+def extractRefOds():
 	res = {}
 	if getattr(sys, 'frozen', False):
             dir_ = os.path.dirname(sys.executable)
@@ -66,10 +97,16 @@ def extractAll():
 	for file in os.listdir(refundDir):
 		if file.endswith(".txt"):
 			lines = refundInfo(refundDir+file)
-			lines += orderInfo(refundDir+file, lines)
 			if not lines:
 				continue			
 			res.update(getRefund(lines))
+
+	for file in os.listdir(refundDir):
+		if file.endswith(".txt"):
+			lines = orderInfo(refundDir + file, res)
+			if not lines:
+				continue
+			res.update(getOriOd(lines, res))
 
 	return res
 
